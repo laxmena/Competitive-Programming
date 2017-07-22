97331fa6fe1394dd7922cb074c95f1b654678a61
Lakshmanan Meiyappan <lakshmanan.meiyappan@gmail.com>
2017-07-23 00:46:23 +0530

    Add practice problems to CodeChef directory

---
+++ CodeChef/CIELAB.cpp
@@ -0,0 +1,103 @@
+//https://www.codechef.com/problems/CIELAB
+
+/*
+ *  Author: Lakshmanan Meiyappan
+ *  Handle: Codechef	: lax2804
+ *			HackerRank	: lax2804
+ *			HackerEarth	: @lakshmanan.meiyappan
+ *			SPOJ		: laxmena
+ *
+ *			GitHub		: https://github.com/laxmena			
+ *	
+ *			Website		: https://laxmena.github.io/website/
+ *
+ *			Email		: lakshmanan.meiyappan@gmail.com
+ *			Attribution : https://github.com/laxmena/Competitive-Programming/blob/master/Template/template.cpp
+ *	
+ */
+
+
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <sstream>
+#include <numeric>
+#include <utility>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define SI(t) 	scanf("%d",&t)
+#define SL(t) 	scanf("%ld",&t)
+#define SLL(t) 	scanf("%lld",&t)
+#define SC(t) 	scanf("%c",&t)
+#define SS(t) 	cin>>t
+#define SF(t) 	scanf("%f",&t)
+#define SLF(t) 	scanf("%lf",&t)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(container) container.begin(), container.end()
+#define rall(container)	container.end(), container.begin()
+#define RFOR(i, from, till, dec) for(int i=from; i>=till; i-=dec)
+#define FOR(i, from, till, inc) for(int i=(from); i<(till); i+=(inc))
+#define FOREACH(it, container) for(auto it=container.begin(); it != container.end(); it++)
+
+#define MP 		make_pair
+#define PB 		push_back
+#define MAX 	(int)1e9
+#define EPS 	1e-9
+#define PI 		3.1415926535897932384626433832795
+#define MOD 	1000000007
+
+const double pi=acos(-1.0);
+typedef vector<int> VI;
+typedef vector<float> VF;
+typedef vector<bool> VB;
+typedef vector<string> VS;
+typedef list<int> LI;
+typedef list<string> LS;
+typedef set<int> SETI;
+typedef set<char> SETC;
+typedef set<float> SETF;
+typedef stack<int> STACKI;
+typedef stack<char> STACKC;
+typedef stack<float> STACKF;
+typedef queue<int> QI;
+typedef queue<char> QC;
+typedef queue<float> QF;
+typedef map<int,int> MAPII;
+typedef map<int,char> MAPIC;
+typedef map<char,int> MAPCI;
+typedef long int l;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int  ull;
+
+int main(){
+	int a,b,diff;
+	SI(a);
+	SI(b);
+
+	diff = a-b;
+	if(diff%10 == 9)
+		printf("%d",diff-1);
+	else
+		printf("%d",diff+1);
+	return 0;
+}
---
+++ CodeChef/HORSES.cpp
@@ -0,0 +1,124 @@
+/*
+ *  Author: Lakshmanan Meiyappan
+ *  Handle: Codechef	: lax2804
+ *			HackerRank	: lax2804
+ *			HackerEarth	: @lakshmanan.meiyappan
+ *			SPOJ		: laxmena
+ *
+ *			GitHub		: https://github.com/laxmena			
+ *	
+ *			Website		: https://laxmena.github.io/website/
+ *
+ *			Email		: lakshmanan.meiyappan@gmail.com
+ *			Attribution : https://github.com/laxmena/Competitive-Programming/blob/master/Template/template.cpp
+ *	
+ */
+
+
+#include <functional>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <sstream>
+#include <numeric>
+#include <utility>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define SI(t) 	scanf("%d",&t)
+#define SL(t) 	scanf("%ld",&t)
+#define SLL(t) 	scanf("%lld",&t)
+#define SC(t) 	scanf("%c",&t)
+#define SS(t) 	cin>>t
+#define SF(t) 	scanf("%f",&t)
+#define SLF(t) 	scanf("%lf",&t)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(container) container.begin(), container.end()
+#define rall(container)	container.end(), container.begin()
+#define RFOR(i, from, till, dec) for(i=from; i>=till; i-=dec)
+#define FOR(i, from, till, inc) for(i=(from); i<(till); i+=(inc))
+#define FOREACH(it, container) for(it=container.begin(); it != container.end(); it++)
+
+#define MP 		make_pair
+#define PB 		push_back
+#define MAX 	(int)1e9
+#define EPS 	1e-9
+#define PI 		3.1415926535897932384626433832795
+#define MOD 	1000000007
+
+const double pi=acos(-1.0);
+typedef vector<int> VI;
+typedef vector<float> VF;
+typedef vector<bool> VB;
+typedef vector<string> VS;
+typedef vector<long> VL;
+typedef list<int> LI;
+typedef list<string> LS;
+typedef set<int> SETI;
+typedef set<char> SETC;
+typedef set<float> SETF;
+typedef set<long> SETL;
+typedef set<long long> SETLL;
+typedef stack<int> STACKI;
+typedef stack<char> STACKC;
+typedef stack<float> STACKF;
+typedef queue<int> QI;
+typedef queue<char> QC;
+typedef queue<float> QF;
+typedef map<int,int> MAPII;
+typedef map<int,char> MAPIC;
+typedef map<char,int> MAPCI;
+typedef long int l;
+typedef unsigned long int ul;
+typedef long long int ll;
+typedef unsigned long long int  ull;
+
+//https://www.codechef.com/problems/HORSES
+int main(){
+	l i,n,t,temp,diff,tempDiff;
+	SL(t);
+	VL speed;
+	while(t-->0){
+		SL(n);
+		REP(i,n){
+			SL(temp);
+			speed.PB(temp);
+		}
+		sort(all(speed));
+		diff = MAX;
+		FOR(i,1,n,1){
+			tempDiff = speed[i]-speed[i-1];
+			if(diff>tempDiff) diff=tempDiff;
+		}
+		cout<<diff<<endl;
+		speed.clear();
+	}
+	return 0;
+}
+
+/*
+3
+5
+4 9 1 32 13
+6
+1 10000000 10000000 89 87234 34
+3
+234 345 457
+*/
